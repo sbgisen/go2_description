0068838face8951b35ae58960d65ea13d1b4998b
gakutasu <gakutasu@gmail.com>
2026-07-16 20:40:36 +0900

    style: Remove trailing whitespace

--- launch/robot_gui.launch.py
+++ launch/robot_gui.launch.py
@@ -55,7 +55,7 @@ def generate_launch_description():
         [
             PathJoinSubstitution([FindExecutable(name='xacro')]),
             ' ',
-            PathJoinSubstitution([FindPackageShare('go2_description'), 
+            PathJoinSubstitution([FindPackageShare('go2_description'),
                                   'robots', description_file]),
             ' ',
             'prefix:=', prefix
